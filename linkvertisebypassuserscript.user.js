d36e8fee6dfee94d21d062c1f8768d806b94626c
Shehajeez <141225264+shehajeez@users.noreply.github.com>
2024-06-16 15:12:25 +0200

    Update linkvertisebypassuserscript.user.js

--- linkvertisebypassuserscript.user.js
+++ linkvertisebypassuserscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Ethos (BYPASS.VIP SRC) LINKVERTISE BYPASSER
 // @namespace     goatbypassers.xyz
-// @version       1.3
+// @version       1.4
 // @description   Credits to BYPASS.VIP for the main script, modified it to use the Ethos API and changed a few messages, alerts. Credits to https://github.com/bypass-vip/userscript/blob/main/bypass-vip.user.js
 // @match         *://*.linkvertise.com/*
 // @match         *://linkvertise.com/*/*
@@ -87,8 +87,8 @@
     }
 
     function updateButtonText(buttonTextEl) {
-        if (buttonTextEl.innerText !== '🗝️ Unlocked successfully!') {
-            buttonTextEl.innerText = '🗝️ Unlocked successfully!';
+        if (buttonTextEl.innerText !== '🗝️ Click me for the result!') {
+            buttonTextEl.innerText = '🗝️ Click me for the result!';
         }
     }
 
